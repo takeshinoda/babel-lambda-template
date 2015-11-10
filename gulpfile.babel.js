05882e742854fa8fb2b500474bd6777acd227c21
Takeshi SHINODA <takeshinoda@gmail.com>
2015-11-11 03:37:43 +0900

    Fixup task sequence.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,40 +2,38 @@ import gulp  from 'gulp'
 import babel from 'gulp-babel'
 import mocha from 'gulp-mocha'
 import file  from 'gulp-file'
+import runSequence from 'run-sequence'
 
 import configure from './build_src/configure'
 
 const SOURCES    = 'src/**/*.js'
 const TEST_FILES = 'test/**/*.js'
 
-gulp.task('test', (done) => {
-  gulp.src(TEST_FILES)
-      .pipe(babel())
-      .pipe(gulp.dest('test_dist'))
-      .pipe(mocha({ reporter: 'nyan' }))
-      .on('end', () => done())
+gulp.task('test', () => {
+  return gulp.src(TEST_FILES)
+             .pipe(babel())
+             .pipe(gulp.dest('test_dist'))
+             .pipe(mocha({ reporter: 'nyan' }))
 })
 
-gulp.task('compile', ['configure'], (done) => {
-  gulp.src(SOURCES)
-      .pipe(babel())
-      .pipe(gulp.dest('dist'))
-      .on('end', () => done())
+gulp.task('compile', ['configure'], () => {
+  return gulp.src(SOURCES)
+             .pipe(babel())
+             .pipe(gulp.dest('dist'))
 })
 
 gulp.task('configure', () => {
   let env = process.env.AP_ENV ? process.env.AP_ENV : 'test'
   let src = configure.build(env)
 
-  file('config.js', `module.exports = ${JSON.stringify(src)}\n`)
-    .pipe(gulp.dest('dist'))
-    .on('end', () => done())
+  return file('config.js', `module.exports = ${JSON.stringify(src)}\n`, { src: true })
+         .pipe(gulp.dest('dist'))
 })
- 
-gulp.task('default', ['compile', 'test'])
 
-gulp.task('watch', (cb) => {
-  gulp.watch(SOURCES, ['compile', 'test'])
-      .on('end', cb)
+gulp.task('default', (cb) => runSequence('compile', 'test', cb))
+
+gulp.task('watch', (done) => {
+  gulp.watch(SOURCES, ['default'])
+      .on('end', done)
 })
 
